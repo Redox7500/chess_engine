c70be6c4dc204b1b224ad2b3869ec58d6c4e025c
redox7500 <redox7500@gmail.com>
2026-05-30 11:48:07 -0700

    Moves stuff around

---
+++ build/CMakeFiles/generate_precomputed_bitboards.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_precomputed_bitboards.dir/generate_precomputed_bitboards.cpp.o"
+  "CMakeFiles/generate_precomputed_bitboards.dir/generate_precomputed_bitboards.cpp.o.d"
+  "generate_precomputed_bitboards"
+  "generate_precomputed_bitboards.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generate_precomputed_bitboards.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/generate_precomputed_bitboards.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for generate_precomputed_bitboards.
+# This may be replaced when dependencies are built.
